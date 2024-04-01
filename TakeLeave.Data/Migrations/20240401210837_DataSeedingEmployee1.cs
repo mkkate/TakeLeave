889d5ee61ebba80d62a11f2e8b75aca1dd33168a
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-01 23:20:21 +0200

    migrations: adding two employees and their days off data

---
+++ TakeLeave.Data/Migrations/20240401210837_DataSeedingEmployee1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TakeLeave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedingEmployee1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SET IDENTITY_INSERT DaysOffs ON " +
+                "INSERT INTO DaysOffs " +
+                "(ID, Vacation, Paid, Unpaid, SickLeave) " +
+                "VALUES " +
+                $"(1, {22}, {5}, {0}, {11})");
+
+            migrationBuilder.Sql($"INSERT INTO Employees " +
+                "(FirstName, LastName, Address, IDNumber, EmploymentStartDate, EmploymentEndDate, InsertDate, DeleteDate, DaysOffID," +
+                " PositionID, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp," +
+                " PhoneNumber, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount, IsAdmin) " +
+                "VALUES " +
+                $"('Maria','Evans','Boelhof 1723','16837308',GETUTCDATE(),GETUTCDATE(),GETUTCDATE(),NULL,{1},{1},'maria.evans','MARIA.EVANS'," +
+                $"'maria.evans@gmail.com','MARIA.EVANS@GMAIL.COM',1,'AQAAAAIAAYagAAAAECuVyIumpN/a0/D5PAS3Ynila7mmRmPHRBoCu7BCns6RRI4+Ub9baxgtiYR3QL9vGw=='," +
+                $"'{Guid.NewGuid()}','{Guid.NewGuid()}','0600000000',1,0,NULL,1,0,1)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ TakeLeave.Data/Migrations/20240401211319_DataSeedingEmployee2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TakeLeave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedingEmployee2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SET IDENTITY_INSERT DaysOffs ON " +
+                "INSERT INTO DaysOffs " +
+                "(ID, Vacation, Paid, Unpaid, SickLeave) " +
+                "VALUES " +
+                $"(2, {20}, {5}, {0}, {11})");
+
+            migrationBuilder.Sql($"INSERT INTO Employees " +
+                "(FirstName, LastName, Address, IDNumber, EmploymentStartDate, EmploymentEndDate, InsertDate, DeleteDate, DaysOffID," +
+                " PositionID, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, PasswordHash, SecurityStamp, ConcurrencyStamp," +
+                " PhoneNumber, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnd, LockoutEnabled, AccessFailedCount, IsAdmin) " +
+                "VALUES " +
+                $"('Emma','Scott','Boulevard Sur Salazar 4098','165547308',GETUTCDATE(),GETUTCDATE(),GETUTCDATE(),NULL,{2},{2},'emma.scott','EMMA.SCOTT'," +
+                $"'emma.scott@gmail.com','EMMA.SCOTT@GMAIL.COM',1,'AQAAAAIAAYagAAAAEJ3LslypDuJfd9xso65trCKXqShi1dn6gi9pv/oP+rZi+8gza9Pos6P4ZavxbrjEIQ=='," +
+                $"'{Guid.NewGuid()}','{Guid.NewGuid()}','0600065100',1,0,NULL,1,0,1)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
