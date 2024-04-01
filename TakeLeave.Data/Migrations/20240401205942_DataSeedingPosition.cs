96c8fbf2d76acd88bd16c3bd7b057a6d88c4f8cb
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-01 23:07:06 +0200

    created migration for seeding positions: Hr Manager and Sowtfware Developer

---
+++ TakeLeave.Data/Migrations/20240401205942_DataSeedingPosition.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TakeLeave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedingPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("SET IDENTITY_INSERT Positions ON " +
+                "INSERT INTO Positions " +
+                "(ID, Title, Description, SeniorityLevel) " +
+                "VALUES " +
+                "(1, 'HR Manager', 'Oversees all HR tasks, including recruiting, training, payroll, benefits and employee relations', 2)");
+
+            migrationBuilder.Sql("SET IDENTITY_INSERT Positions ON " +
+                "INSERT INTO Positions " +
+                "(ID, Title, Description, SeniorityLevel) " +
+                "VALUES " +
+                "(2, 'Software Developer','Designing software for businesses and consumers alike, working closely with clients to determine what they need', 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
