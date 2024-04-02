666c9f8baf710b09fda0f090a8135b4077d611ff
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-02 15:26:25 +0200

    feat: added roles Admin, User and HR

---
+++ TakeLeave.Data/Migrations/20240401212127_DataSeedEmployeeRoles.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TakeLeave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedEmployeeRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("SET IDENTITY_INSERT EmployeeRoles ON " +
+                "IF NOT EXISTS (SELECT 1 FROM EmployeeRoles WHERE Name = 'Admin') " +
+                "INSERT INTO EmployeeRoles " +
+                "(Id, Name, NormalizedName, ConcurrencyStamp) " +
+                "VALUES " +
+                "(1, 'Admin', 'ADMIN', NULL)");
+
+            migrationBuilder.Sql("SET IDENTITY_INSERT EmployeeRoles ON " +
+                "IF NOT EXISTS (SELECT 1 FROM EmployeeRoles WHERE Name = 'User') " +
+                "INSERT INTO EmployeeRoles " +
+                "(Id, Name, NormalizedName, ConcurrencyStamp) " +
+                "VALUES " +
+                "(2, 'User', 'USER', NULL)");
+
+            migrationBuilder.Sql("SET IDENTITY_INSERT EmployeeRoles ON " +
+                "IF NOT EXISTS (SELECT 1 FROM EmployeeRoles WHERE Name = 'HR') " +
+                "INSERT INTO EmployeeRoles " +
+                "(Id, Name, NormalizedName, ConcurrencyStamp) " +
+                "VALUES " +
+                "(3, 'HR', 'HR', NULL)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
