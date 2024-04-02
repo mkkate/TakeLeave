f873def04d6fe2fe8b0d609ee33d3aa6a09d73be
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-02 15:27:44 +0200

    feat: added userroles for userids 1 and 2

---
+++ TakeLeave.Data/Migrations/20240401212802_DataSeedEmployeeUserRoles.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TakeLeave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedEmployeeUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("IF NOT EXISTS (SELECT 1 FROM EmployeeUserRoles WHERE UserId = 1 AND RoleId = 1) " +
+                "INSERT INTO EmployeeUserRoles " +
+                "(UserId, RoleId) " +
+                "VALUES " +
+                "(1, 1)");
+
+            migrationBuilder.Sql("UPDATE Employees " +
+                "SET IsAdmin = 0 " +
+                "WHERE Id = 2 ");
+
+            migrationBuilder.Sql("IF NOT EXISTS (SELECT 1 FROM EmployeeUserRoles WHERE UserId = 2 AND RoleId = 2) " +
+                "INSERT INTO EmployeeUserRoles " +
+                "(UserId, RoleId) " +
+                "VALUES " +
+                "(2, 2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
