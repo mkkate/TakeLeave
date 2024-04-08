3ef5584f9de36659cbdc869e65bbcbe4dac88a78
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-08 22:50:39 +0200

    feat: created login using Identity for checking signed user and assigned role

---
+++ TakeLeave.Web/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TakeLeave.Data.Database.Employees;
+using TakeLeave.Web.Models;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace TakeLeave.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<Employee> _signInManager;
+        private readonly UserManager<Employee> _userManager;
+
+        public LoginController(SignInManager<Employee> signInManager, UserManager<Employee> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                SignInResult? result = await _signInManager.PasswordSignInAsync(
+                    loginViewModel.Input.UserName,
+                    loginViewModel.Input.Password,
+                    isPersistent: false,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    Employee? employee = await _userManager.FindByNameAsync(loginViewModel.Input.UserName);
+
+                    if (employee != null)
+                    {
+                        bool userRole = await _userManager.IsInRoleAsync(employee, "User");
+
+                        return userRole ? RedirectToAction("Index", "User") : RedirectToAction("Index", "Hr");
+                    }
+                }
+            }
+
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ TakeLeave.Web/Models/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TakeLeave.Web.Models
+{
+    public class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel()
+        {
+            [Required]
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+    }
+}
