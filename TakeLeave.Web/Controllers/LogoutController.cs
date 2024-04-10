2e7f17008eff585eef616e2d59edc0d52d4eff9c
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-10 14:11:19 +0200

    feat: create Logout functionality

---
+++ TakeLeave.Web/Controllers/LogoutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TakeLeave.Data.Database.Employees;
+
+namespace TakeLeave.Web.Controllers
+{
+    public class LogoutController : Controller
+    {
+        private readonly SignInManager<Employee> _signInManager;
+
+        public LogoutController(SignInManager<Employee> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnLogout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("OnLogin", "Login");
+        }
+    }
+}
