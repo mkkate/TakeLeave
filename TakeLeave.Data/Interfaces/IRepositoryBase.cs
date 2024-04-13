0196b955f74853b5f72777a7dc99a14ccf1c9705
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-13 15:26:11 +0200

    feat: created base repository

---
+++ TakeLeave.Data/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace TakeLeave.Data.Interfaces
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
---
+++ TakeLeave.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TakeLeave.Data.Interfaces;
+
+namespace TakeLeave.Data.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected TakeLeaveDbContext DbContext { get; set; }
+
+        public RepositoryBase(TakeLeaveDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return DbContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression)
+        {
+            return DbContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Insert(T entity)
+        {
+            DbContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            DbContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            DbContext.Set<T>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
