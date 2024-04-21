d0b748883a991fe0cece16cbf6f7659c9fdfc83b
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2024-04-21 15:16:06 +0200

    fix: modified employee UserName and Email to NOT NULL

---
+++ TakeLeave.Data/Migrations/20240421130359_ModifyNullableValues.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TakeLeave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyNullableValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE Employees ALTER COLUMN UserName NVARCHAR(256) NOT NULL;");
+            migrationBuilder.Sql("ALTER TABLE Employees ALTER COLUMN Email NVARCHAR(256) NOT NULL;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
